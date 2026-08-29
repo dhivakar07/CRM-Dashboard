b6a195d601907698a652c2e84f279346c499a710
dhivakar07 <dhivakar782001@gmail.com>
2026-08-29 09:28:53 +0530

    updata server.js in backend

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const jsonServer = require("json-server");
 const path = require("path");
 
 const server = jsonServer.create();
-
 const router = jsonServer.router(path.join(__dirname, "data.json"));
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
 
 module.exports = server;
